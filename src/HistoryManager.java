d4d48cf8bcafd698ce34990ba267999e563997a4
Roman Yegorov <YegrovRoman32@yandex.ru>
2026-02-27 20:06:40 +0300

    Исправил форматирование

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -2,8 +2,11 @@ import java.util.List;
 import java.util.Map;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     List<Task> getHistory();
-    void remove (int id);
+
+    void remove(int id);
 
 }
