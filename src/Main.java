57e43e3625798b77f4ca4415380d1e5909ce0b7a
Roman Yegorov <YegrovRoman32@yandex.ru>
2026-03-13 20:17:52 +0300

    Исправил оформление

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,8 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        String file ="D://test/TestFile.CSV";
+        String file = "D://test/TestFile.CSV";
         FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(new File(file));
-
 
         Task task1 = new Task("Сделать уроки", "Написать сочинение");
         int taskId1 = manager.createNewTask(task1);
